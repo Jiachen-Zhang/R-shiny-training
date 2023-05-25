08d421dd7a713f4c2d2a635d464a822dadfd02e2
Jiachen-Zhang <Jiachen.zhang@n-actuarial.com>
2023-05-25 11:47:39 +0800

    version 1.1

--- app.R
+++ app.R
@@ -41,9 +41,10 @@ server <- function(input, output, session) {
     df <- claims.data()
     number_loss_year <- n_distinct(df$`Loss Year`)
     number_dev_year <- max(df$`Development Year`)
-    triangle <- matrix(0, nrow = number_loss_year, ncol = number_dev_year, byrow = TRUE)
     loss_years <- sort(unique(df$`Loss Year`))
     dev_years <- c(1:number_dev_year)
+    
+    triangle <- matrix(0, nrow = number_loss_year, ncol = number_dev_year, byrow = TRUE)
     rownames(triangle) <- loss_years
     colnames(triangle) <- dev_years
     
@@ -73,15 +74,21 @@ server <- function(input, output, session) {
   
   claims.plot <- reactive({
     full.triangle <- claims.triangle()
-    Development = rep(c(1:(max(df$`Development Year`)+1)), times = number_loss_year)
-    Loss = as.character(rep(loss_years, each = max(df$`Development Year`)+1))
+    df <- claims.data()
+    number_loss_year <- n_distinct(df$`Loss Year`)
+    number_dev_year <- max(df$`Development Year`)
+    loss_years <- sort(unique(df$`Loss Year`))
+    dev_years <- c(1:number_dev_year)
+    
+    Development = rep(c(1:(number_dev_year+1)), times = number_loss_year)
+    Loss = as.character(rep(loss_years, each = number_dev_year+1))
     Amount = round(c(t(full.triangle)))
     
     df_plot = data.frame(Development, Loss, Amount)
     ggplot(df_plot, aes(x=Development, y=Amount, group=Loss, color=Loss)) +
       geom_point(size = 1.5) +
       geom_text(label=Amount, hjust=0.5, vjust=-1, 
-                check_overlap = TRUE, size = 3, color = "black") +
+                check_overlap = TRUE, size = 4, color = "black") +
       geom_line(linewidth = 0.5) +
       xlab("Development Year") +
       ylab("Amount ($)") +
